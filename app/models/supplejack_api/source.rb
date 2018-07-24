072f4ec72b69d438a1bfcd5e68819a4fc8b9fcb7
Reinkaos <richard@boost.co.nz>
2018-07-25 09:41:33 +1200

    Attempting to work out query

--- app/models/supplejack_api/source.rb
+++ app/models/supplejack_api/source.rb
@@ -21,12 +21,24 @@ module SupplejackApi
     # Its not completely random. Its not effiient to run .sample on large collections.
     # Fetches 4 random records from first 100 and last 100
     def random_records(limit)
-      records = Record.where('fragments.source_id' => source_id, :status => 'active')
 
-      first_hundred = records.sort('fragments.syndication_date' => 1).limit(100).to_a
-      last_hundred = records.sort('fragments.syndication_date' => -1).limit(100).to_a
+      # db = Mongoid::Clients.default
+      # collection = db[:records]
+      # collection.aggregate([ { '$sample': { size: 10 }}]).to_a
 
-      (first_hundred | last_hundred).sample(limit)
+      # .where('fragments.source_id' => source_id, status: 'active')
+
+
+      # This doesn't quite work, it appears to fetch all of the records and then sample them..
+
+      Record.aggregates([{ '$match': { source_id: source_id, status: 'active' }, '$sample': { size: limit } }])
+
+      # records = Record.where('fragments.source_id' => source_id, :status => 'active')
+      #
+      # first_hundred = records.sort('fragments.syndication_date' => 1).limit(100).to_a
+      # last_hundred = records.sort('fragments.syndication_date' => -1).limit(100).to_a
+      #
+      # (first_hundred | last_hundred).sample(limit)
     end
   end
 end
